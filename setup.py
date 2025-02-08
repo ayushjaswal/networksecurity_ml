48b2383414b5df95751929d755a9340106ff0272
Ayush Jaswal <ayushjaswal4543@gmail.com>
2025-02-08 20:59:03 +0530

    type[add]: setup

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
- 
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+  '''
+  Parse requirements.txt file and return a list of requirements.
+  '''
+  requirement_lst: List[str] = []
+  try:
+    with open('requirements.txt', 'r') as f:
+      lines = f.readlines()
+
+      # Process each line to remove comments and whitespace
+      for line in lines:
+        requirement= line.strip() # Ignore whitespaces 
+        if (requirement and requirement != '-e .' ): # Ignore empty lines and '-e.' lines
+          requirement_lst.append(requirement) # Append to the requirement list
+
+          
+  except FileNotFoundError:
+    print('requirements.txt file not found.')
+  
+  return requirement_lst
+
+setup(
+  name="NetworkSecurity",
+  version="0.0.1",
+  author="Ayush",
+  author_email="ayushjaswal4543@gmail.com",
+  packages=find_packages(),
+  install_requires=get_requirements() 
+)
